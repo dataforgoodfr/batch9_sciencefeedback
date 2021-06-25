e6e436b8e31c0e2849403a16996ab0e958b78614
Eledoux <lawanledoux@gmail.com>
2021-06-25 18:37:41 +0200

    remove postgres

--- api/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# pylint: disable=C0415
-# pylint: disable=W0611
-
-def import_models():
-    from models.researcher import Researcher
--- api/sandboxes/creators/ci/create_or_modify_researchers.py
+++
@@ -1,9 +0,0 @@
-from repository.researchers import create_or_modify_researcher
-
-
-def create_or_modify_researchers():
-    create_or_modify_researcher({
-        'email': 'max.foo@gmail.com',
-        'firstName': 'Max',
-        'lastName': 'Foo'
-    })
